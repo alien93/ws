7a7fbf276ed3e5ff8ddcb7fcc9f1a8e299de6b46
Nina Marjanovic <nina.marjanovic@gmail.com>
2016-05-15 21:11:19 +0200

    Task priority and status

--- public/controllers/adminTaskController.js
+++ public/controllers/adminTaskController.js
@@ -9,14 +9,14 @@ angular.module('tsApp')
             var comment1 = {"author":"Pera Peric", "commentDate":"25.5.2016.","content":"Komentar1"};
             var comment2 = {"author":"asdf123", "commentDate":"26.5.2016.","content":"Komentar2"};
 	    var tasks = [{"name":"Task1", "contributor": taskContributor, "percentage": 30,
-                          "status":"To Do", "description":"Task1 description", "comments": [comment1, comment2]}, 
+                          "status":"To Do", "priority":"Minor", 
+                          "description":"Task1 description", "comments": [comment1, comment2]}, 
 			 {"name":"Task2","contributor": taskContributor, "percentage": 50,
-                          "status":"To Do", "description": "Task2 description", "comments": [comment1]}];
+                          "status":"To Do", "priority":"Major", 
+                          "description": "Task2 description", "comments": [comment1]}];
 	    var project = {"name" : "proj1", "contributors" : contributors, "tasks":tasks};
             //-------------------\test data------------------
+           
             $scope.projects = [project];
-            
-            $scope.task = $scope.projects[idProj].tasks[idTask];		
-            
-            console.log($scope.task);
+            $scope.task = $scope.projects[idProj].tasks[idTask];
         });
